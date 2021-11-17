ab12c7c7187042533ff71de3b3ce2c042e472528
Leonardo Landin <leonardo.simoes@reclameaqui.com.br>
2021-11-17 09:35:28 -0300

    init

---
+++ src/main/java/com/example/back02/Back02Application.java
@@ -0,0 +1,13 @@
+package com.example.back02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Back02Application.class, args);
+    }
+
+}
